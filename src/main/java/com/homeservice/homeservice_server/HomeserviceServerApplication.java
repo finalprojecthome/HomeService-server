4eb7f0a725bc32166f2e8dfcd3ad0d835484ffb0
Anuson <anusornthongpaenchai@gmail.com>
2026-03-26 13:52:51 +0700

    Setup Springboot and Add Dockerfile for Render deployment

---
+++ src/main/java/com/homeservice/homeservice_server/HomeserviceServerApplication.java
@@ -0,0 +1,13 @@
+package com.homeservice.homeservice_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeserviceServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeserviceServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/homeservice/homeservice_server/HomeserviceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homeservice.homeservice_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeserviceServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
