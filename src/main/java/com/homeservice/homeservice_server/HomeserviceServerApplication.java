659fd12032d2ed19340d8b2a5947057dd5b707f7
Sorrawit Amdaeng <sorrawit.amdaeng@gmail.com>
2026-04-01 18:22:55 +0700

    feat: add CORS configuration support with CorsProperties and WebConfig for enhanced security

--- src/main/java/com/homeservice/homeservice_server/HomeserviceServerApplication.java
+++ src/main/java/com/homeservice/homeservice_server/HomeserviceServerApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
+import com.homeservice.homeservice_server.config.CorsProperties;
 import com.homeservice.homeservice_server.config.SupabaseProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(SupabaseProperties.class)
+@EnableConfigurationProperties({ SupabaseProperties.class, CorsProperties.class })
 public class HomeserviceServerApplication {
 
 	public static void main(String[] args) {
